edd7e75c740c91c7167204fd5f7f71ea681cb258
sanketshejul76 <sanketshejul76@gmail.com>
2023-07-21 18:14:30 +0530

    changes

--- index.js
+++ index.js
@@ -35,7 +35,8 @@ TYPESCRIPT
 */
 app.use(cors({
     // origin: "https://eriosbooking-production.up.railway.app",
-    origin: "https://eriosbooking-e2oinmw9f-sanketshejul76.vercel.app/?vercelToolbarCode=AdyPpi44BD5CxmB",
+    // origin: "https://eriosbooking-e2oinmw9f-sanketshejul76.vercel.app/?vercelToolbarCode=AdyPpi44BD5CxmB",
+    origin: "https://alive-rose-squirrel.cyclic.app",
     // origin: "http://localhost:5173",
     credentials: true
 }))
